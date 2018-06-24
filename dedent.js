50d3ff5b19b8ea14489fcd2e67ed33942ee67a73
mister. jones <JonesEffect@Gmail.com>
2018-06-24 02:04:35 -0400

    Removed `assembleTemplateLiteral()` from utils.js.
    Replaced it with the native `String.raw()`, as it is an equivalent function.

--- dedent.js
+++ dedent.js
@@ -5,27 +5,31 @@ import {
 } from './utils.js'
 
 /**
- * Removes code-formatting indentation from multiline template/string literals.  
+ * Removes code-formatting indentation from multiline template/string literals.
  * Can be called as a "standard" function or as a tag function.
  * @param {(string|string[])} strings A multiline String or an Array of Strings via a tag function.
  * @param {*} [keys] Template expressions via a tag function.
- * 
+ *
  * @returns {string} Multiline String with corrected indentation.
  */
 const dedent = function dedentMultilineTemplateOrStringLiteral(strings, ...keys) {
-  const string = (isTemplateStringObject(strings))
+  const string = (
+    isTemplateStringObject(strings)
     ? String.raw(strings, ...keys)
     : strings
-  
+  )
+
 	const lines = splitIntoLines(string)
 	const baselineIndentation = minimumLeadingWhitespaceOf(lines)
-	
-  const dedentedString =
+
+  const dedentedString = (
     lines
       .map(line => line.substr(baselineIndentation))
       .join('\n')
-  
+  )
+
   return dedentedString
 }
 
 export default dedent
+
